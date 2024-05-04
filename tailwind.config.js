d49bbe0a51fc41838ae4f26fa7f0f565e3c642e6
Ashish <160389529+Ashish05122004@users.noreply.github.com>
2024-05-04 23:17:52 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+
+    extend: {
+      fontFamily: {
+        mullish: ['mulish', 'sans-serif']
+      },
+      colors: {
+        darkblue: "#02042a"
+      },
+    },
+  },
+  plugins: [],
+}
+
